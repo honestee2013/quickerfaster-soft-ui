81bebb6a9b4be6410c6e844bf2c1fe0cd6dbee74
Aminu A Mohammed <honestee2013@gmail.com>
2026-02-13 06:43:28 +0100

    Comment moreActions on the time, people & policy modules  to avoid undefine route errors

--- app/Modules/System/Resources/views/dashboard-manager.blade.php
+++
@@ -1,9 +0,0 @@
-<x-qf::livewire.bootstrap.layouts.dashboards.default-dashboard>
-    <x-slot name="mainTitle">
-        <strong class="text-info text-gradient">Dashboard</strong>
-    </x-slot>
-
-
-    <h3> This page is coming soon...</h3>
-
-</x-qf::livewire.bootstrap.layouts.dashboards.default-dashboard>
